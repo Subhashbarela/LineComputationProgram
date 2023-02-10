cde7a4f95a9084d4697254bb09c12a8c8ad47013
Subhash <barelasubhsh16@gmail.com>
2023-02-10 17:02:37 +0530

    Findout length between two point of line

---
+++ DistanceBtwnTwoLine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComputationProgram
+{
+    internal class DistanceBtwnTwoLine
+    {
+        public static void length(double x1, double y1, double x2, double y2)
+        {
+            double Distance = Math.Sqrt((x2 - x1) * 2 + (y2 - y1) * 2);
+            Console.WriteLine("Length Between two Line is : " + Distance);
+        }
+    }
+}
